2f2b270628ea3fc4aeb593b777a182b81b280d6e
RipplerES <Richard.doucet@ripplerES.com>
2017-03-02 17:39:25 -0500

    stuff missed in last checkin

---
+++ src/SimpleToDo/Models/ToDoItem/Complete.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleToDo.Models.ToDoItem
+{
+    public class Complete
+    {
+        public Guid Id { get; set; }
+        public int Version { get; set; }
+    }
+
+}
---
+++ src/SimpleToDo/Models/ToDoItem/DescriptionUpdate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleToDo.Models.ToDoItem
+{
+    public class DescriptionUpdate
+    {
+        public Guid Id { get; set; }
+        public int Version { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class NewToDoItem
+    {
+        public string Description { get; set; }
+    }
+}
